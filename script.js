89079475b6eed98ec57997320ed0b9cdec3bb56c
Prachi <pn71900@gmail.com>
2024-06-14 17:16:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// ====================== typing animation===========>
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Backend Developer","Software Developer"],
+    typeSpeed:90,
+    BackSpeed:50,
+    loop:true
+})
